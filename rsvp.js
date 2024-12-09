ca7efa2e21603298711aeb8ef3f861e65516327b
BimanditoHerlambang <bimanditoherlambang@gmail.com>
2024-12-09 18:47:45 +0700

    Add files via upload

---
+++ rsvp.js
@@ -0,0 +1,5 @@
+document.getElementById("openInvitation").addEventListener("click", function () {
+    document.querySelector(".intro-screen").classList.add("hidden");
+    document.querySelector(".main-invitation").classList.remove("hidden");
+  });
+  
